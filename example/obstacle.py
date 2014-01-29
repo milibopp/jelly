7449867b054e281a214a5d167775f961a6c8e576
Eduard Bopp <eduard.bopp@aepsil0n.de>
2014-01-29 15:15:18 +0100

    Update example to current codebase

---
+++ example/obstacle.py
@@ -0,0 +1,32 @@
+"""
+jelly example script - background gas with obstacle
+
+This script demonstrates the use of a circular obstacle within the initial
+conditions.
+
+"""
+
+from jelly.ics import write_icfile
+from jelly.model import Mesh, UniformGas
+from jelly.util import CartesianGrid2D, Rectangle, CircularObstacle
+from jelly.vector import Vector
+
+
+# Setup gas and grid
+gas = UniformGas()
+grid = CartesianGrid2D(
+        Rectangle(Vector(0.0, 0.0), Vector(1.0, 1.0)),
+        (32, 32))
+
+# Setup a circular obstacle
+circle = CircularObstacle(
+        center=Vector(1.0, 1.0),
+        radius=0.1,
+        n_phi=100)
+
+# Combine everything into a mesh
+mesh = Mesh(gas, grid, [circle], boxsize=2.0)
+
+# Write the mesh to a file
+with open('obstacle.dat', 'wb') as icfile:
+    write_icfile(icfile, mesh)
